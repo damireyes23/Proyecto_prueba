5648c1badb34b440446f189f2af516ca79eade28
damireyes23 <damian.reyes.23@gmail.com>
2022-07-21 15:09:07 +0200

    Cambios en validaciones JS y comentarios

--- script.js
+++ script.js
@@ -4,44 +4,64 @@ const correo = document.getElementById("correo")
 const telefono = document.getElementById("telefono")
 const form = document.getElementById("formjs")
 const parrafo = document.getElementById("warnings")
+const warningnombre = document.getElementById("warningname")
+const warningcorreo = document.getElementById("warningmail")
+const warningtelefono = document.getElementById("warningphone")
+
 
 //CANCELAR EVENTO POR DEFECTO
 form.addEventListener("submit", e=>{
     e.preventDefault()
-//REINICIAR AVISOS
+
+    //REINICIAR AVISOS
     let warnings = ""
-//CREAR VARIABLE BOOLEANA
+    let warningname = ""
+    let warningmail = ""
+    let warningphone = ""
+
+    //CREAR VARIABLE BOOLEANA
     let entrar = false
-//CREAR EXPRESIONES REGULARES
-    let regexEmail = /([a-zA-Z0-9]+)([.{1}])?([a-zA-Z0-9]+)@(yahoo|hotmail|outlook|gmail)([.])com/
-    let regexTelefono = /^\d{10,10}$/
+
+    //CREAR EXPRESIONES REGULARES
+    let regexEmail = /([a-zA-Z0-9]+)([.{1}])?([a-zA-Z0-9]+)@(yahoo|hotmail|outlook|gmail|YAHOO|HOTMAIL|OUTLOOK|GMAIL)([.])com/
+    let regexTelefono = /$^|^\d{10,10}$/
     let regexNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
-//IMPRIMIR MENSAJE DE AVISO
+
+    //IMPRIMIR MENSAJE DE AVISO
     parrafo.innerHTML = ""
-//CREAR CONDICIONES EN BASE A LAS EXPRESIONES REGULARES    
+    warningnombre.innerHTML = ""
+    warningcorreo.innerHTML = ""
+    warningtelefono.innerHTML = ""
+
+//CREAR CONDICIONES EN BASE A LAS EXPRESIONES REGULARES
+
     if(!regexNombre.test(nombre.value)){
-        warnings += `El nombre ingresado es incorrecto. Solo se admiten letras y espacios. <br>`
+        warningname += `El nombre ingresado es incorrecto (solo se admiten letras y espacios). <br>`
         entrar = "true"
     }
     if(!regexEmail.test(correo.value)){
-    warnings += `El email ingresado no es válido (solo se admiten dominios de GMAIL, YAHOO y Outlook). <br>`
+    warningmail += `El correo ingresado no es válido (solo se admiten dominios de GMAIL, YAHOO y Outlook). <br>`
     entrar = true
     }
-    if(!regexTelefono.test (telefono.value)){
-        warnings += `Su teléfono debe contener 10 caracteres (solo se permiten números). <br>`
+    if(!regexTelefono.test(telefono.value)){
+        warningphone += `Su teléfono debe contener 10 caracteres (solo se admiten números). <br>`
         entrar = true
     }
-//MOSTRAR AVISOS EN VARIABLE WARNINGS
+
+    //MOSTRAR AVISOS EN VARIABLE WARNINGS
     if(entrar){
-        parrafo.innerHTML = warnings}
-//CREAR CONDICIONAL PARA MOSTRAR QUE LOS DATOS FUERON VALIDADOS CORRECTAMENTE
+        parrafo.innerHTML = warnings
+        warningnombre.innerHTML = warningname
+        warningcorreo.innerHTML = warningmail
+        warningtelefono.innerHTML = warningphone
+    }
+
+    //CREAR CONDICIONAL PARA MOSTRAR QUE LOS DATOS FUERON VALIDADOS CORRECTAMENTE
     else{
         parrafo.innerHTML = "Enviado! Nos comunicaremos con usted a la brevedad!"
+        document.getElementById("formjs").reset();
     }
 })
-
-
-
 
 
 //CONTAR CARACTERES DEL FORMULARIO DE MENSAJE
