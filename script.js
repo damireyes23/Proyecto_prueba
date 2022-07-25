99812fab35d7d1927c228282247c43b67880043e
damireyes23 <damian.reyes.23@gmail.com>
2022-07-25 15:36:09 +0200

    version 1.0

--- script.js
+++ script.js
@@ -1,78 +1,44 @@
-//SELECCIONAR ELEMENTOS HTML
-const nombre = document.getElementById("nombre")
-const correo = document.getElementById("correo")
-const telefono = document.getElementById("telefono")
-const form = document.getElementById("formjs")
-const parrafo = document.getElementById("warnings")
-const warningnombre = document.getElementById("warningname")
-const warningcorreo = document.getElementById("warningmail")
-const warningtelefono = document.getElementById("warningphone")
+const input = document.querySelector("searchinput")
+const button = document.querySelector("button")
+const postcontainer = document.querySelector("post-container")
 
+function traerPost(){
+    fetch("https://jsonplaceholder.typicode.com/posts/1") 
+    
+    .then ((res) => res.json())
+    .then((data) => {
+        crearPost(data)
+ })
+}
 
-//CANCELAR EVENTO POR DEFECTO
-form.addEventListener("submit", e=>{
-    e.preventDefault()
+function crearPost(mostrar){
+    const titulouser = document.createElement ("h1")
+    titulouser.textContent = ("User ID:")
 
-    //REINICIAR AVISOS
-    let warnings = ""
-    let warningname = ""
-    let warningmail = ""
-    let warningphone = ""
+    const h1 = document.createElement("h1")
+    h1.textContent = mostrar.userId
 
-    //CREAR VARIABLE BOOLEANA
-    let entrar = false
+    const tituloid = document.createElement ("h1")
+    tituloid.textContent = ("ID:")
+    
+    const h3 = document.createElement("h3")
+    h3.textContent = mostrar.id
 
-    //CREAR EXPRESIONES REGULARES
-    let regexEmail = /([a-zA-Z0-9]+)([.{1}])?([a-zA-Z0-9]+)@(yahoo|hotmail|outlook|gmail|YAHOO|HOTMAIL|OUTLOOK|GMAIL)([.])com/
-    let regexTelefono = /$^|^\d{10,10}$/
-    let regexNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/
+    const titulopost = document.createElement ("h3")
+    titulopost.textContent = ("Titulo Post:")
 
-    //IMPRIMIR MENSAJE DE AVISO
-    parrafo.innerHTML = ""
-    warningnombre.innerHTML = ""
-    warningcorreo.innerHTML = ""
-    warningtelefono.innerHTML = ""
+    const encabezadopost = document.createElement("h3")
+    encabezadopost.textContent = mostrar.title
 
-//CREAR CONDICIONES EN BASE A LAS EXPRESIONES REGULARES
+    const div= document.createElement("div")
+    div.appendChild (titulouser) 
+    div.appendChild (h1) 
+    div.appendChild (tituloid)
+    div.appendChild (h3)
+    div.appendChild (titulopost)
+    div.appendChild (encabezadopost)
 
-    if(!regexNombre.test(nombre.value)){
-        warningname += `El nombre ingresado es incorrecto (solo se admiten letras y espacios). <br>`
-        entrar = "true"
-    }
-    if(!regexEmail.test(correo.value)){
-    warningmail += `El correo ingresado no es válido (solo se admiten dominios de GMAIL, YAHOO y Outlook). <br>`
-    correo.style.border = '1px solid red'
-    entrar = true
-    }
-    if(!regexTelefono.test(telefono.value)){
-        warningphone += `Su teléfono debe contener 10 caracteres (solo se admiten números). <br>`
-        entrar = true
-    }
+    post_container.appendChild(div)
+}
 
-    //MOSTRAR AVISOS EN VARIABLE WARNINGS
-    if(entrar){
-        parrafo.innerHTML = warnings
-        warningnombre.innerHTML = warningname
-        warningcorreo.innerHTML = warningmail
-        warningtelefono.innerHTML = warningphone
-    }
-
-    //CREAR CONDICIONAL PARA MOSTRAR QUE LOS DATOS FUERON VALIDADOS CORRECTAMENTE
-    else{
-        parrafo.innerHTML = "Enviado! Nos comunicaremos con usted a la brevedad!"
-        document.getElementById("formjs").reset();
-        correo.style.border = 'none'
-    }
-})
-
-
-//CONTAR CARACTERES DEL FORMULARIO DE MENSAJE
-const mensaje = document.getElementById('mensaje');
-const contador = document.getElementById('contador');
-
-mensaje.addEventListener('input', function(e) {
-    const target = e.target;
-    const longitudMax = target.getAttribute('maxlength');
-    const longitudAct = target.value.length;
-    contador.innerHTML = `${longitudAct}/${longitudMax}`;
-});
+traerPost()
